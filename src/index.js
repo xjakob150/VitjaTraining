175ae3dbc399b6cf58364620312cd2786828d59a
Jakob Lipovec <xjakob150@gmail.com>
2023-09-27 16:56:44 +0200

    LOL

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import "./navbar.css"
+import "./form.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/Kontakt.js
+++ src/pages/Kontakt.js
@@ -1,7 +1,28 @@
 export default function Kontakt() {
     return (<>
     <h1>Kontakt</h1>
-    
+    <div className="form-container">
+        <form>
+            <div className="form-group">
+            <label for="ime">ime:</label>
+            <input type="text" id="ime" name="ime" required/><br/>
+            </div>
+
+            <div className="form-group">
+            <label for="priimek">priimek:</label>
+            <input type="text" id="priimek" name="priimek" required/><br/>
+            </div>
+
+            <div className="form-group">
+            <label for="zadeva">zadeva:</label>
+            <textarea id="zadeva" name="zadeva" rows="4" required></textarea><br/>
+            </div>
+
+            <div className="form-group">
+            <input type="submit" value="Submit"/>
+            </div>
+        </form>
+    </div>
 
     </>)
 }
