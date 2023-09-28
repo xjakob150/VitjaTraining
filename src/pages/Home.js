7c1a84a11fb9e92a332b817134561b289f3d0d8a
Jakob Lipovec <xjakob150@gmail.com>
2023-09-28 19:19:31 +0200

    Popravki

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ export default function Home() {
     return (
     <>
         <div className="container-home">
-                <p className="citat">"Izguba ni opcija. Vsi morajo uspet"</p>
+            <p className="citat">"Izguba ni opcija. Vsi morajo uspeti."</p>
             <img src="./vitja.jpg" alt="Vitja" className="animated-image"/>
             <p className="citat">Arnold Schwarzenegger</p>
         </div>
