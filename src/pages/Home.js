bb39146fb92ad8ecaf1dce2a8bdcbaa5baec1edf
Jakob Lipovec <xjakob150@gmail.com>
2023-10-30 10:49:46 +0100

    LOL

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,12 @@
 export default function Home() {
     return (
     <>
-        <div className="container-all">
             <div classname="container-home">
                 <h1>Greatness pulls mediocrity into the mud</h1>
             </div>
             <div className="container-home">
                 <img src="./Vitja1.jpg" alt="Vitja" className="animated-image"/>
             </div>
-        </div>
     </>
     )
 }
